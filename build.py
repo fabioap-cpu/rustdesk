00e77c1fb11d74cc6d5de735d943f5a50bf95834
fabioap-cpu <68659468+fabioap-cpu@users.noreply.github.com>
2023-10-16 16:17:36 +0100

    Update build.py

--- build.py
+++ build.py
@@ -428,17 +428,17 @@ def build_flutter_windows(version, features):
     os.chdir('libs/portable')
     system2('pip3 install -r requirements.txt')
     system2(
-        f'python3 ./generate.py -f ../../{flutter_build_dir_2} -o . -e ../../{flutter_build_dir_2}/fuckdesk.exe')
+        f'python3 ./generate.py -f ../../{flutter_build_dir_2} -o . -e ../../{flutter_build_dir_2}/rustdesk.exe')
     os.chdir('../..')
-    if os.path.exists('./fuckdesk_portable.exe'):
-        os.replace('./target/release/fuckdesk-portable-packer.exe',
-                   './fuckdesk_portable.exe')
+    if os.path.exists('./rustdesk_portable.exe'):
+        os.replace('./target/release/rustdesk-portable-packer.exe',
+                   './rustdesk_portable.exe')
     else:
-        os.rename('./target/release/fuckdesk-portable-packer.exe',
-                  './fuckdesk_portable.exe')
+        os.rename('./target/release/rustdesk-portable-packer.exe',
+                  './rustdesk_portable.exe')
     print(
-        f'output location: {os.path.abspath(os.curdir)}/fuckdesk_portable.exe')
-    os.rename('./fuckdesk_portable.exe', f'./fuckdesk-{version}-install.exe')
+        f'output location: {os.path.abspath(os.curdir)}/rustdesk_portable.exe')
+    os.rename('./rustdesk_portable.exe', f'./fuckdesk-{version}-install.exe')
     print(
         f'output location: {os.path.abspath(os.curdir)}/fuckdesk-{version}-install.exe')
 
